4bd53b444e977de20b9f6fb6daebe8f4c647c68f
KoEonYack <21300035@handong.edu>
2020-05-22 13:31:45 +0900

    Updates

---
+++ precache-manifest.2567f7adaf8193a2df495266f759acb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a5e0cd974859e94c92928d0c68de3b",
+    "url": "/react-prize-app/index.html"
+  },
+  {
+    "revision": "5f0dd9d751e6e41e7ce9",
+    "url": "/react-prize-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4e94cf7d5fa42c7c8e6e",
+    "url": "/react-prize-app/static/js/2.3db81ecb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-prize-app/static/js/2.3db81ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0dd9d751e6e41e7ce9",
+    "url": "/react-prize-app/static/js/main.aa137667.chunk.js"
+  },
+  {
+    "revision": "5a769ded93a23ea9d270",
+    "url": "/react-prize-app/static/js/runtime-main.2d99a3c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-prize-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
